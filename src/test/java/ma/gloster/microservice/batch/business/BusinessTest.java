7d6240c62c98a7552e679730a30c6c6e4a41663f
soukaina <soukainadahar@gmail.com>
2018-09-07 11:04:32 +0100

    [userInJob]

--- src/test/java/ma/gloster/microservice/batch/business/BusinessTest.java
+++ src/test/java/ma/gloster/microservice/batch/business/BusinessTest.java
@@ -1,5 +1,7 @@
 package ma.gloster.microservice.batch.business;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +14,7 @@ public class BusinessTest {
 
 	@Test
 	public void updateUserTest() throws BusinessException {
-
+		assertEquals("true", "true");
 	}
 
 }
