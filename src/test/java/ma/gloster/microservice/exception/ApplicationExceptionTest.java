030905125170389772cdc385a6117f3296137e86
unknown <theyounessfathi@gmail.com>
2018-09-11 23:57:27 +0100

    [prohet with mockito test]

---
+++ src/test/java/ma/gloster/microservice/exception/ApplicationExceptionTest.java
@@ -0,0 +1,32 @@
+package ma.gloster.microservice.exception;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+/**
+ * The Class ApplicationExceptionTest.
+ */
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ApplicationExceptionTest {
+	
+	@Mock
+	private TestRestTemplate restTemplate;
+	
+	/**
+	 * Perform user in job with exception test.
+	 */
+	@Test
+	public void performUserInJobWithExceptionTest(){
+		ApplicationException body = this.restTemplate.postForObject("/userInJobRequest/batch@devops.com/1232", String.class,
+				ApplicationException.class);
+		when(body).thenReturn(new ApplicationException());
+	}
+
+}
