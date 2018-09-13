8d17e9fa97cf3d351ef5d4ffc0850446dc0c4dd1
unknown <theyounessfathi@gmail.com>
2018-09-13 10:52:53 +0100

    [project with test properties file]

--- src/test/java/ma/gloster/microservice/business/UserBusinessImpTest.java
+++ src/test/java/ma/gloster/microservice/business/UserBusinessImpTest.java
@@ -1,8 +1,10 @@
 package ma.gloster.microservice.business;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-
+import java.util.Arrays;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,10 +13,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 
-import ma.gloster.microservice.business.IUserBusiness;
 import ma.gloster.microservice.repository.UserRepository;
 import ma.gloster.microservice.repository.entity.UserEntity;
-import static org.mockito.Mockito.*;
 
 /**
  * The Class UserBusinessImpTest.
@@ -27,15 +27,20 @@ public class UserBusinessImpTest {
 	/** The user repository. */
 	@Mock
 	private UserRepository userRepository;
-
+	
 	/**
 	 * Update user test.
 	 */
 	@Test
 	public void updateUserTest() {
-		UserEntity user1 = new UserEntity("addresstest", "statustest", "emailtest");
+		UserEntity user1 = new UserEntity("addressTest", "statusTest", "emailTest");
 		this.userRepository.save(user1);
-		this.userRepository.updateUser("addresstestupdate", "addresstestupdate", "emailtestupdate");
+		this.userRepository.updateUser("addressTestUpdate", "addressTestUpdate", "emailTest");
+
+		UserEntity user2 = new UserEntity("addressTestUpdate", "statusTestUpdate", "emailTest");
+
+		when(userRepository.findAll()).thenReturn(Arrays.asList(user2));
+
 		java.util.List<UserEntity> users = this.userRepository.findAll();
 		for (UserEntity user : users) {
 			assertNotEquals(user1.getAdress(), user.getAdress());
