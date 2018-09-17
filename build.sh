cac4e06035048a0a7f7e56616ac3887db306dd6a
soukaina <soukainadahar@gmail.com>
2018-09-17 18:48:28 +0100

    Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${TRAVIS_BRANCH} = 'develop' ]]; then
-    echo 'Checking Quality Gates'
-    mvn -B clean verify sonar:sonar  -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=gloster-e -Dsonar.organization=marocraft -Dsonar.login=${SONAR_LOGIN};
+      echo 'Checking Quality Gates'
+      mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=d9d4bfd1b8c2cbbb223457adc37d73e78ad168da -Dsonar.projectKey=gloster-e -Dsonar.organization=marocraft 
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
-    echo 'Build and analyze pull request'
-    mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.projectKey=gloster-e -Dsonar.organization=marocraft  -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=8f65232cf6a6b66ee03ead0a87eae8c0eb84b024 -Dsonar.pullrequest.github.repository=gloster-e/master -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+      echo 'Build and analyze pull request'
+      mvn -B clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io  -Dsonar.projectKey=gloster-e -Dsonar.organization=marocraft  -Dsonar.login=9d4bfd1b8c2cbbb223457adc37d73e78ad168da -Dsonar.github.oauth= 87c1aa5b6ae95f5937e5b4562f636c4d512737af -Dsonar.pullrequest.github.repository=marocraft/gloster-e -Dsonar.pullrequest.provider=GitHub  -Dsonar.pullrequest.branch=${TRAVIS_BRANCH}  -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
 fi
