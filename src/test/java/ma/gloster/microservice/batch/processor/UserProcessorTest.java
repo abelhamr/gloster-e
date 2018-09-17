be7a5f54acbb9d62dd5f5438391887ceddc6b880
soukaina <soukainadahar@gmail.com>
2018-09-17 19:01:05 +0100

    process test

--- src/test/java/ma/gloster/microservice/batch/processor/UserProcessorTest.java
+++ src/test/java/ma/gloster/microservice/batch/processor/UserProcessorTest.java
@@ -1,27 +1,18 @@
 package ma.gloster.microservice.batch.processor;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import ma.gloster.microservice.dto.UserDto;
-
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-@PropertySource("classpath:application.properties")
 public class UserProcessorTest {
-	
+
 	@Test
-	public void processTest() throws Exception{
-//		UserProcessorImpl userProcessorImpl = new UserProcessorImpl();
-//		assertNotNull(userProcessorImpl.process(new UserDto()));
+	public void processTest() throws Exception {
+		// UserProcessorImpl userProcessorImpl = new UserProcessorImpl();
+		// assertNotNull(userProcessorImpl.process(new UserDto()));
 	}
 
 }
